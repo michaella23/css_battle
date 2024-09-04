278d91998bc0ff0451ae587ad11c32e16a92b3aa
Michaella Rodriguez <micha.ella@hotmail.com>
2024-09-04 12:59:50 -0500

    header and styles

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 const mainContainer = document.getElementById("main-container")
 const solutionsOfTheDay = document.querySelectorAll(".solution-of-the-day")
 
-// create a loop
-// loop through all of the files and get the index.html
-// create an element on the main page of the iste
-
-// anchor tag with an of href="day[i]/index.html"
-const screenshot = "screenshot.png"
-
 for (let i=0; i<30; i++) {
     mainContainer.innerHTML += `
         <div class="solution-of-the-day">
-            <div class="placeholder"></div>
+            <div class="placeholder">` + // this '+' will be removed along with extra backticks
+            // <img src=day${[i]}/solution.png alt="" class="solution-img" />
+            // once solution screenshots have been added to each folder, can render on main page
+            `</div>
             <a href=day${[i]}/index.html>Day ${i+1}</a>
         </div>
     `
