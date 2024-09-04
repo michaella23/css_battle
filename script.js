e89117e8d8611d0557bc554809b903297558d63f
Michaella Rodriguez <micha.ella@hotmail.com>
2024-09-03 19:51:29 -0500

    added placeholder image for daily solutions

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ const solutionsOfTheDay = document.querySelectorAll(".solution-of-the-day")
 // create an element on the main page of the iste
 
 // anchor tag with an of href="day[i]/index.html"
+const screenshot = "screenshot.png"
 
 for (let i=0; i<30; i++) {
     mainContainer.innerHTML += `
-        <a href=day${[i]}/index.html>Day ${i + 1}</a>
+        <div class="solution-of-the-day">
+            <img src="screenshot.png" alt="lime green background with the word screenshot in a black Arial font"/>
+            <a href=day${[i]}/index.html>Day ${i + 1}</a>
+        </div>
     `
 }
