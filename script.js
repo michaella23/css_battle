42f238807dc447d1b3947ceaecae63e700dd0cda
Michaella Rodriguez <micha.ella@hotmail.com>
2024-09-04 19:00:26 -0500

    created src folder to hold daily repos

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const solutionsOfTheDay = document.querySelectorAll(".solution-of-the-day")
 
 for (let i=1; i<=30; i++) {
 
-    const imgPath = `day${[i]}/solution.png`
+    const imgPath = `src/day${[i]}/solution.png`
     const solutionEl = document.createElement("div")
     solutionEl.setAttribute("class", "solution-of-the-day")
     const placeholderEl = document.createElement("div")
     placeholderEl.setAttribute("class", "placeholder")
     const anchorEl = document.createElement("a")
-    anchorEl.href = `day${[i]}/index.html`
+    anchorEl.href = `src/day${[i]}/index.html`
     anchorEl.textContent = `Day ${i}`
     const imageEl = document.createElement("img")
 
